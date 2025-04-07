d5e33cff94077730a02624eff1fb231e9c436041
Nathan <nchen6@cps.edu>
2025-04-07 10:39:26 -0500

    Completed

--- a7.py
+++ a7.py
@@ -70,8 +70,19 @@ class BayesClassifier:
         #     <the rest of your code for updating frequencies here>
             text = self.load_file(os.path.join(self.training_data_directory, filename))
             token = self.tokenize(text)
-            print(token)
+            # print(token)
 
+            if filename.startswith(self.pos_file_prefix):
+                self.update_dict(token, self.pos_freqs)
+            elif filename.startswith(self.neg_file_prefix):
+                self.update_dict(token, self.neg_freqs)
+
+        print(self.pos_freqs["awesome"])
+        print(self.neg_freqs["awesome"])
+        print(self.pos_freqs["great"])
+        print(self.neg_freqs["great"])
+        print(self.pos_freqs["the"])
+        print(self.neg_freqs["the"])
 
         # we want to fill pos_freqs and neg_freqs with the correct counts of words from
         # their respective reviews
@@ -100,6 +111,10 @@ class BayesClassifier:
         # avoid extra work in the future (using the save_dict method). The objects you
         # are saving are self.pos_freqs and self.neg_freqs and the filepaths to save to
         # are self.pos_filename and self.neg_filename
+        self.save_dict(self.pos_freqs, self.pos_filename)
+        self.save_dict(self.neg_freqs, self.neg_filename)
+
+
 
     def classify(self, text: str) -> str:
         """Classifies given text as positive, negative or neutral from calculating the
@@ -112,7 +127,7 @@ class BayesClassifier:
             classification, either positive, negative or neutral
         """
         # TODO: fill me out
-
+        print(self.pos_freqs)
         
         # get a list of the individual tokens that occur in text
         
@@ -224,7 +239,12 @@ class BayesClassifier:
             freqs - dictionary of frequencies to update
         """
         # TODO: your work here
-        pass  # remove this line once you've implemented this method
+        # print("update dict")
+        for word in words:
+            if word in freqs:
+                freqs[word] += 1
+            else:
+                freqs[word] = 1
 
 
 if __name__ == "__main__":
